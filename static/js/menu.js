d8faf0abdb6be252b72c5ae20fe94c8482b21a9a
kp4ws <kentpawson123@gmail.com>
2021-06-10 11:09:29 -0600

    Updated Hamburger menu and added an X icon. Starting to work on About section now

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,6 +1,10 @@
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.getElementById('nav-links');
 
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
+function hamburgerMenu()
+{
+    hamburger.innerHTML = navLinks.classList.contains("active") ? "<i class='fas fa-bars'></i>" : "<i class='fas fa-times'></i>";
+    navLinks.classList.toggle('active');
+}
+
+hamburger.addEventListener('click', hamburgerMenu, false);
