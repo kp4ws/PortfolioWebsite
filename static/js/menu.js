d83c375c096c1b8935fad7f85e6fba88cafcdf8a
kp4ws <kentpawson123@gmail.com>
2021-10-04 16:38:27 -0600

    Minor changes to menu script

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,18 +1,20 @@
 "use strict";
 
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
 
-function toggleMenu()
+
+function toggleMenu(nav)
 {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = nav ?? document.getElementById('nav-links');
+
     hamburger.innerHTML = navLinks.classList.contains("active") ? "<i class='icon-font fas fa-bars'></i>" : "<i class='icon-font fas fa-times'></i>";
     navLinks.classList.toggle('active');
 }
 
-function toggleNav() {
-    if (!navLinks.classList.contains("active"))
+function toggleNav(nav) {
+    if (!nav.classList.contains("active"))
     {
         return;
     }
-    toggleMenu();
+    toggleMenu(nav);
 }
